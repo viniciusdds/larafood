9de31f9401a5a8fad33f3c3092e45d5415ea62a1
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-05-16 21:47:03 -0300

    segunda commit

---
+++ app/Http/Controllers/Admin/PlanController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Plan;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlanController extends Controller
+{
+    private $repository;
+
+    public function __construct(Plan $plan)
+    {
+        $this->repository = $plan;
+    }
+
+    public function index(){
+        $plans = $this->repository->latest()->paginate();
+
+        return view('admin.pages.plans.index', compact('plans'));
+    }
+
+    public function create()
+    {
+        return view('admin.pages.plans.create');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $data = $request->all();
+        $data['url'] = Str::kebab($request->name);
+        $this->repository->create($data);
+
+        return redirect()->route('plans.index');
+    }
+}
