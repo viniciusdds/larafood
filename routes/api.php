f0c24b4cf8cca74c88a4cb8f38146491dbcbf73f
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-06-23 17:42:33 -0300

    Modulo 19 aula 10

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/tenants/{uuid}', 'Api\TenantApiController@show');
-Route::get('/tenants', 'Api\TenantApiController@index');             
+Route::get('/tenants', 'Api\TenantApiController@index');
+
+Route::get('/categories', 'Api\CategoryApiController@categoriesByTenant');
