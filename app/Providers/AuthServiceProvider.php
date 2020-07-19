d82951017dd64f5d885611f0813f0bd703cb6594
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-07-19 17:41:42 -0300

    modulo 22 aula 3

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,6 +29,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        if($this->app->runningInConsole()){
+            return;
+        }
+
         $permissions = Permission::all();
 
         foreach ($permissions as $permission) {
