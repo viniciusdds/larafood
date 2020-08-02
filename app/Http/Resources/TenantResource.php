162cc360ac64f8c1803c1fb0b93c68d38fd19e2d
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-08-02 16:12:49 -0300

    modulo 23 aula 5

--- app/Http/Resources/TenantResource.php
+++ app/Http/Resources/TenantResource.php
@@ -15,24 +15,18 @@ class TenantResource extends JsonResource
      */
     public function toArray($request)
     {
-<<<<<<< HEAD
         //return parent::toArray($request);
         return [
             'name' => $this->name,
             'image' => $this->logo ? url("storage/{$this->logo}") : null,
-=======
-        return [
             'name' => $this->name,
             'image' => $this->logo ? url("storage/{$this->logo}") : '',
->>>>>>> master
             'uuid' => $this->uuid,
             'flag' => $this->url,
             'contact' => $this->email,
             'date_created' => Carbon::parse($this->created_at)->format('d-m-Y'),
         ];
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
+
+
